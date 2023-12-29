e19f4f991c84e4509202e4ef2df2f439c899b306
adel <adel_5210m@yahoo.com>
2023-12-29 20:51:17 +0800

    Fist commit

---
+++ src/WebSocketComponent.js
@@ -0,0 +1,63 @@
+import React from "react";
+import {v4 as uuidv4} from 'uuid';
+
+const WebSocketComponent = () => {
+    const [socket, setSocket] = React.useState(null);
+    const [message, setMessage] = React.useState('');
+    const [messages, setMessages] = React.useState([]);
+
+    React.useEffect(() => {
+        const newSocket = new WebSocket('ws://localhost:8090/ws')
+
+        newSocket.addEventListener('open', (event) => {
+            console.log('WS connection opened')
+        })
+
+        newSocket.addEventListener('message', (event)=> {
+            const receivedMsg = JSON.parse(event.data)
+            setMessages((prvMessages) => [...prvMessages, receivedMsg])
+        })
+
+        newSocket.addEventListener('close', (event) => {
+            console.log('WS connection closed')
+        })
+
+        setSocket(newSocket)
+
+        return () => newSocket.close()
+    }, [])
+
+    const sendMessage = () => {
+        if (socket && message.trim() !== ''){
+            const newMsg = {
+                id: uuidv4(),
+                data: message,
+            }
+
+            socket.send(JSON.stringify(newMsg))
+
+            setMessage('')
+        }
+    }
+
+    return (
+        <div>
+            <div>
+                <input
+                    type='text'
+                    value={message}
+                    onChange={(e) => setMessage(e.target.value)}
+                />
+                <button
+                    onClick={sendMessage}>Send</button>
+            </div>
+            <ul>
+                {messages.map((msg) => (
+                    <li key={msg.id}>{msg.data}</li>
+                ))}
+            </ul>
+        </div>
+    )
+}
+
+export default WebSocketComponent
