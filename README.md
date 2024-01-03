e29c85e563e9aa970186ddaaa4d0328a112bd7a4
adel <adel_5210m@yahoo.com>
2024-01-03 15:13:32 +0800

    Updated deployment.yaml

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 - docker run -p 3000:3000 go-ws-kafka-ui
 ### Push to docker hub
 - docker images
-- docker tag go-ws-kafka-ui adel5210/go-ws-kafka-ui:1.0.0
-- docker push adel5210/go-ws-kafka-ui:1.0.0
+- docker tag go-ws-kafka-ui adel5210/go-ws-kafka-ui:1.0.2
+- docker push adel5210/go-ws-kafka-ui:1.0.2
 ### Kube setup attempt 1
 - kubectl apply -f k8s/deployment.yaml 
--- src/WebSocketComponent.js
+++ src/WebSocketComponent.js
@@ -8,8 +8,7 @@ const WebSocketComponent = (props) => {
     const [messages, setMessages] = React.useState([]);
 
     React.useEffect(() => {
-        const newSocket = new WebSocket('ws://0.0.0.0:8090/ws')
-        // const newSocket = new WebSocket('ws://192.168.49.2:32276/ws')
+        const newSocket = new WebSocket('ws://go-ws-kafka-service:8090/ws')
 
         newSocket.addEventListener('open', (event) => {
             console.log('WS connection opened')
